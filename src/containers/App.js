db84c8f706c2e004b4406865095a588d4e0dd3e4
Bruno <bruno.umbelino0@gmail.com>
2021-04-13 21:15:52 -0300

    adding test - mew task

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import Todo from "../components/Todo";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>TodoList</h1>
+        <Todo />
+      </header>
+    </div>
+  );
+};
+
+export default App;
