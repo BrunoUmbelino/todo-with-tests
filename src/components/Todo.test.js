7ba61ea754887c69704458e4adcff67ac3b1d0f5
Bruno <bruno.umbelino0@gmail.com>
2021-04-14 19:56:45 -0300

    adding test for title

--- src/components/Todo.test.js
+++ src/components/Todo.test.js
@@ -3,10 +3,12 @@ import React from "react";
 import { fireEvent, render, waitFor } from "@testing-library/react";
 
 import Todo from "./Todo";
+import App from "../containers/App";
 
 describe("Tests for Todo component", () => {
   it("Should add new task when form has been submitted", async () => {
     const { getByTestId, getByText } = render(<Todo />);
+
     const fieldNode = await waitFor(() => getByTestId("form-field"));
     const newTask = "testing";
     fireEvent.change(fieldNode, { target: { value: newTask } });
@@ -17,5 +19,13 @@ describe("Tests for Todo component", () => {
 
     const tdNode = await waitFor(() => getByText(newTask));
     expect(tdNode).toBeDefined();
+  });
+});
+
+describe("Tests for App", () => {
+  it("Title equal TodoList", () => {
+    const app = render(<App />);
+    const title = app.getByTestId("title");
+    expect(title.innerHTML).toEqual("TodoList");
   });
 });
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TodoList</h1>
+        <h1 data-testid="title">TodoList</h1>
         <Todo />
       </header>
     </div>
